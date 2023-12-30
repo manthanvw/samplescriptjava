3121b5e583156209eedc408900729f79035f26d4
manthanvw <118655224+manthanvw@users.noreply.github.com>
2023-12-30 11:12:33 +0530

    Update App.java

--- src/main/java/in/kmit/ExampleJavaProject/App.java
+++ src/main/java/in/kmit/ExampleJavaProject/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello i did changes" );
+        System.out.println( "Hello Changes made by Manthan" );
     }
 }
